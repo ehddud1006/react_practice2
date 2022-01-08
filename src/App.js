37996a9bcd034659c1d33f58f79ae262311e0c70
ehddud1006 <62373865+ehddud1006@users.noreply.github.com>
2022-01-08 18:38:09 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+const name = "Angela";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello {name}</h1>
+
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
